297db22360be8265e8508b418d391c85547650eb
Ivan Archakov <battleweaver@gmail.com>
2020-05-31 18:00:14 +0300

    empty board test

--- src/test/com/battleweaver/GameTest.java
+++ src/test/com/battleweaver/GameTest.java
@@ -1,22 +1,36 @@
 package test.com.battleweaver;
 
-import static org.junit.jupiter.api.Assertions.*;
+import main.com.battleweaver.Game;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 class GameTest {
-
+    int BOARD_SIZE = 3;
+    Game testGame = new Game("", 0, BOARD_SIZE);
+    Game testHumanGame = new Game("Player1", "Player2", BOARD_SIZE);
     @org.junit.jupiter.api.Test
+    void getBoard() {
+        String [][] empty_board = new String[][] {{" ", " ", " "}, {" ", " ", " "}, {" ", " ", " "}};
+        String [][] created_board = testGame.getBoard();
+
+        Assertions.assertArrayEquals(empty_board, created_board);
+    }
+    @Test
     void start() {
+
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void move() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getWinner() {
+
     }
 
-    @org.junit.jupiter.api.Test
-    void getBoard() {
-    }
+
 }
